60a49a5bea8a9c308390ac2933e5f05921aaa153
Serhii Lisovik <serlesovik@gmail.com>
2024-11-03 22:35:53 +0200

    Added project files

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Portfolio
+
+# Netlify
+
+https://serhii-lisovyk-portfolio.netlify.app/
+
+# Tools
+
+<p align="left">
+
+ <img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/html5-colored.svg" width="40" height="40" alt="HTML5" />
+  
+  <img width="12" />
+  
+  <img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/css3-colored.svg" width="40" height="40" alt="CSS3" />
+
+  <br>
+  <br>
+
+  <img src="https://www.svgrepo.com/show/303440/gulp-logo.svg" width="40" height="40" alt="Gulp" />
+
+  <img width="12" />
+
+  <img src="https://www.cdnlogo.com/logos/n/39/npm-square-red.svg" width="40" height="40" alt="npm" />
+
+  <img width="12" />
+
+  <img src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/git-colored.svg" width="40" height="40" alt="Git" />
+</p>
+
+# How to copy
+
+<p align="left">git clone https://github.com/serlisovyk/Portfolio.git</p>
+<p align="left">npm i</p>
+<p align="left">npm run dev</p>
---
+++ gulp/dev.js
@@ -0,0 +1,138 @@
+import gulp from 'gulp'
+import fileInclude from 'gulp-file-include'
+import server from 'gulp-server-livereload'
+import clean from 'gulp-clean'
+import fs from 'fs'
+import plumber from 'gulp-plumber'
+import notify from 'gulp-notify'
+import changed from 'gulp-changed'
+import svgsprite from 'gulp-svg-sprite'
+import replace from 'gulp-replace'
+
+const plumberNotify = title => {
+  return {
+    errorHandler: notify.onError({
+      title: title,
+      message: 'Error <%= error.message %>',
+      sound: false,
+    }),
+  }
+}
+
+gulp.task('clean:dev', function(done) {
+  if (fs.existsSync('./build/')) {
+    return gulp.src('./build/', { read: false }).pipe(clean({ force: true }))
+  }
+  done()
+})
+
+gulp.task('html:dev', function() {
+  return gulp
+    .src('./src/index.html')
+    .pipe(changed('./build/', { hasChanged: changed.compareContents }))
+    .pipe(plumber(plumberNotify('HTML')))
+    .pipe(
+      fileInclude({
+        prefix: '@@',
+        basepath: '@file',
+      })
+    )
+    .pipe(
+      replace(
+        /(?<=src=|href=|srcset=)(['"])(\.(\.)?\/)*(images|images|fonts|css|scss|sass|js|files|audio|video)(\/[^\/'"]+(\/))?([^'"]*)\1/gi,
+        '$1./$4$5$7$1'
+      )
+    )
+    .pipe(gulp.dest('./build/'))
+})
+
+gulp.task('css:dev', function() {
+  return gulp
+    .src('./src/style.css')
+    .pipe(plumber(plumberNotify('CSS')))
+    .pipe(gulp.dest('./build'))
+})
+
+gulp.task('fonts:dev', function() {
+  return gulp
+    .src('./src/fonts/**/*')
+    .pipe(changed('./build/fonts/'))
+    .pipe(gulp.dest('./build/fonts/'))
+})
+
+gulp.task('images:dev', function() {
+  return gulp
+    .src('./src/images/**/*')
+    .pipe(changed('./build/images/'))
+    .pipe(gulp.dest('./build/images/'))
+})
+
+gulp.task('svgStack:dev', function() {
+  return gulp
+    .src('./src/images/svg/**/*.svg')
+    .pipe(plumber(plumberNotify('SVG:dev')))
+    .pipe(
+      svgsprite({
+        mode: {
+          stack: {
+            example: true,
+          },
+        },
+        shape: {
+          transform: [
+            {
+              svgo: {
+                js2svg: { indent: 4, pretty: true },
+              },
+            },
+          ],
+        },
+      })
+    )
+    .pipe(gulp.dest('./build/images/svgsprite/'))
+})
+
+gulp.task('svgSymbol:dev', function() {
+  return gulp
+    .src('./src/images/svg/**/*.svg')
+    .pipe(plumber(plumberNotify('SVG:dev')))
+    .pipe(
+      svgsprite({
+        mode: {
+          symbol: {
+            sprite: '../sprite.symbol.svg',
+          },
+        },
+        shape: {
+          transform: [
+            {
+              svgo: {
+                js2svg: { indent: 4, pretty: true },
+                plugins: [
+                  {
+                    name: 'removeAttrs',
+                    params: {
+                      attrs: '(fill|stroke)',
+                    },
+                  },
+                ],
+              },
+            },
+          ],
+        },
+      })
+    )
+    .pipe(gulp.dest('./build/images/svgsprite/'))
+})
+
+gulp.task('server:dev', function() {
+  return gulp.src('./build/').pipe(server({ livereload: true, open: true }))
+})
+
+gulp.task('watch:dev', function() {
+  gulp.watch('./src/css/**/*.css', gulp.parallel('css:dev'))
+  gulp.watch(['./src/index.html', './src/html/**/*.json'], gulp.parallel('html:dev'))
+  gulp.watch('./src/images/**/*', gulp.parallel('images:dev'))
+  gulp.watch('./src/fonts/**/*', gulp.parallel('fonts:dev'))
+  gulp.watch('./src/images/svg/*', gulp.series('svgStack:dev', 'svgSymbol:dev'))
+})
